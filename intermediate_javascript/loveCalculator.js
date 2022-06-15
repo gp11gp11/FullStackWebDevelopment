1e06e55f8667de213694492dc303b34167359cad
gp11gp11 <gp7510gp@gmail.com>
2022-06-14 20:17:11 -0700

    comparators

--- intermediate_javascript/loveCalculator.js
+++ intermediate_javascript/loveCalculator.js
@@ -1,22 +1,56 @@
 //Random number generator
 
-/*var n = math.random();
+/*var n = Math.random();
   n = n*6
-  n = math.floor(n) + 1;
+  n = Math.floor(n) + 1;
   console.log(n);
 */
 
 name = prompt("Enter your Name");
 partnerName = prompt("Enter partners name");
 
-var loveScore = math.random();
+var loveScore = Math.random();
 loveScore = loveScore*100;
-loveScore = math.floor(loveScore)+1;
+loveScore = Math.floor(loveScore)+1;
 alert("your love score is " + loveScore + "%");
 
 //controlstatement
 
-if (track == "clear"){
+/*if (track == "clear"){
     gostraight();
    } else {
     turnright();} 
+*/
+
+name = prompt("Enter your Name");
+partnerName = prompt("Enter partners name");
+
+var loveScore = Math.random();
+loveScore = loveScore*100;
+loveScore = Math.floor(loveScore)+1;
+
+if (loveScore == 100){
+    alert("your love score is " + loveScore + "%." + "both of you are made for each other");
+}else{
+    alert("your love score is " + loveScore + "%");
+}
+
+//comparator and equality
+// a===b not only check for equality but also whether data type are equal
+
+//combining comparators
+
+
+name = prompt("Enter your Name");
+partnerName = prompt("Enter partners name");
+
+var loveScore = Math.random();
+loveScore = loveScore*100;
+loveScore = Math.floor(loveScore)+1;
+
+if (loveScore>=80 && loveScore <= 100){
+    alert("your love score is " + loveScore + "%." + "both of you are made for each other");
+}
+if(loveScore>0){
+    alert("your love score is " + loveScore + "%");
+}
