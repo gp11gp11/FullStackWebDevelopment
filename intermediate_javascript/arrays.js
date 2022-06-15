ac956980c0b61a9ef970a12e81cdf80d7dcf3a58
gp11gp11 <gp7510gp@gmail.com>
2022-06-15 09:04:00 -0700

    Array

---
+++ intermediate_javascript/arrays.js
@@ -0,0 +1,9 @@
+
+var guestList = ["job","hani"];
+var guestname = prompt("Enter your name");
+
+if (guestList.includes(guestname)){
+    alert("You are welcome");
+}else{
+    alert("sorry, you are not in the list");
+}
