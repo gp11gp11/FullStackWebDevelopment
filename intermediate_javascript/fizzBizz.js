40ae5c4870ae2c9a0e23e85671aeb95f82fe7123
Godson Prakasia <98373232+gp11gp11@users.noreply.github.com>
2023-01-30 20:49:21 +0530

    Update fizzBizz.js
     fizzbizz program

--- intermediate_javascript/fizzBizz.js
+++ intermediate_javascript/fizzBizz.js
@@ -1,5 +1,28 @@
-
+/*
 var arr = [];
 arr.push(1);
 arr.pop();
 alert(arr);
+*/
+
+var arr = [];
+
+var count = 0;
+
+function fizzBizz() {
+    count += 1;
+   
+
+    if (count%3==0){
+        arr.push("fizz");
+    }else if(count%5==0){
+        arr.push("bizz");
+    }else {
+         arr.push(count);
+    }
+
+    console.log(arr);
+    
+    
+}
+
