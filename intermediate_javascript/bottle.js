db0c53f16c0c46696a82938b02ba651e296787b8
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-03-16 12:56:14 +0530

    99 bottle challenge

---
+++ intermediate_javascript/bottle.js
@@ -0,0 +1,8 @@
+var count = 99;
+
+while(count>=0){
+    next = count-1;
+    console.log(count + "bottles of beer on the wall," + count +" bottle of beer.\n Take one down and pass it around" +next+ "bottle of beer on the wall");
+    count --;
+
+}
