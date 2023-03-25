606ea7d7a918b2ad3454ed1960360deb369dfca9
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-03-26 00:35:40 +0530

    Adding eventlistener in jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -7,3 +7,17 @@ $("button").html("<em>Hey</em>");
 $("img").attr("src"); // To get value of attribute
 
 $("a").attr("href", "https://www.yahoo.com/");//  changing the value of attribute
+
+//adding eventlistener
+$("button").click(function(){
+    $("h1").css("color","purple");
+});
+
+$("input").keypress(function(evemt){
+    // console.log(event.key);
+    $("h1").text(event.key);
+})
+
+$("h1").on("mouseover",function(){
+    $("h1").css("color", "purple");
+});
