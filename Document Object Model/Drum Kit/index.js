ce2fbb2547447ea85793335822dfac90408d9c29
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-03-25 16:49:25 +0530

    Added eventlistner to website to get noticed when key is pressed

--- Document Object Model/Drum Kit/index.js
+++ Document Object Model/Drum Kit/index.js
@@ -5,7 +5,7 @@
 //     alert("got Clicked")
 // }
 
-
+// Added eventlistner to button to get noticed when clicked
 var noOfDrumButons = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i<noOfDrumButons; i++) {
@@ -47,6 +47,47 @@ for(var i = 0; i<noOfDrumButons; i++) {
     }
 
 } );
+
+// Added eventlistner to website to get noticed when key is pressed
+
+    document.addEventListener("keypress", function() {
+        var keyPressed = event.key;
+
+        switch(keyPressed) {
+            case "w":
+                var tom1 = new Audio("/Volumes/external_ssd/myProjects/fullStackDevelopment/FullStackWebDevelopment/Document Object Model/Drum Kit/sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("/Volumes/external_ssd/myProjects/fullStackDevelopment/FullStackWebDevelopment/Document Object Model/Drum Kit/sounds/tom-2.mp3");
+                tom2.play();
+                break;
+                
+            case "s":
+                var tom3 = new Audio("/Volumes/external_ssd/myProjects/fullStackDevelopment/FullStackWebDevelopment/Document Object Model/Drum Kit/sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("/Volumes/external_ssd/myProjects/fullStackDevelopment/FullStackWebDevelopment/Document Object Model/Drum Kit/sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio("/Volumes/external_ssd/myProjects/fullStackDevelopment/FullStackWebDevelopment/Document Object Model/Drum Kit/sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var crash = new Audio("/Volumes/external_ssd/myProjects/fullStackDevelopment/FullStackWebDevelopment/Document Object Model/Drum Kit/sounds/crash.mp3");
+                crash.play();
+                break;
+            case "l":
+                var kick = new Audio("/Volumes/external_ssd/myProjects/fullStackDevelopment/FullStackWebDevelopment/Document Object Model/Drum Kit/sounds/kick-bass.mp3");
+                kick.play();
+                break;
+    
+            default: console.log(keyPressed);
+        }
+
+    } );
     
 }
 
