225d731c2d87cc6ece3e472d028d5a0a76cfa4a6
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-03-26 00:22:26 +0530

    How to view and add attribute to element

--- jQuery/index.js
+++ jQuery/index.js
@@ -3,3 +3,7 @@ $("h1").addClass("big-title margin-50");
 $("h1").text("bye");
 
 $("button").html("<em>Hey</em>");
+
+$("img").attr("src"); // To get value of attribute
+
+$("a").attr("href", "https://www.yahoo.com/");//  changing the value of attribute
