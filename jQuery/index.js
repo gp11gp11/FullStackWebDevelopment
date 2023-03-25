b6a3c212fc6f2de131ac2d4218085fa3b15ccba2
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-03-26 00:55:41 +0530

    website Animation with jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -32,4 +32,19 @@ $("h1").prepend("<button>new</button>");
 
 $("h1").append("<button>new</button>");
 
-$("button").remove();
+// $("button").remove();
+
+// website Animation with jQuery
+
+$("button").on("click", function( ) {
+    // $("h1").hide();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeIn();
+    // $("h1").fadeToggle();
+    // $("h1").slideUp();
+    // $("h1").slideToggle();
+    // $("h1").animate({opacity:0.5});
+    $("h1").slideUp().slideDown().animate({opacity:0.5});
+})
+// $("h1").show();
