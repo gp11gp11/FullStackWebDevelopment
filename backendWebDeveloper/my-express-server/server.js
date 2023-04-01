b2d047c489b062755f49d5358f0f57c5ac3c44d0
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-04-01 16:45:58 +0530

    added routes

--- backendWebDeveloper/my-express-server/server.js
+++ backendWebDeveloper/my-express-server/server.js
@@ -1,5 +1,9 @@
 //jshint eversion:6
 
+//nodemon.io - help to reload automatically
+//npm install -g nodemon
+// in terminal - nodemon server.js - to run
+
 const express = require("express");
 const app =  express(); //function that represent express module
 // to specify if request happens
@@ -8,6 +12,13 @@ app.get("/",function(request, response){
     response.send("<h1>Hello world</h1>");
 });
 
+app.get("/contact",function(req, res){
+    res.send("contact me at: name@gmail.com");
+});
+
+app.get("/about",function(req, res){
+    res.send("I am a computer science student");
+});
 
 app.listen(3000, function(){// access from browser - localhost:3000
     console.log("server started at port 3000");
