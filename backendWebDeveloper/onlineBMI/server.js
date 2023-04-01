cd512105973efbf4c13d483a1ef7088d8209bf71
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-04-01 18:53:54 +0530

    Online BMI calculator

---
+++ backendWebDeveloper/onlineBMI/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app =  express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+    
+app.post("/", function(req, res){
+    // console.log(req.body.num1);// get in text 
+    var num1 = Number(req.body.height);
+    var num2 = Number(req.body.weight);
+    var result = (num2/(num1*num1))*10000;
+    
+    res.send("your bmi is "+ Math.floor(result));// to get the post - npm install body-parser
+});
+app.listen(3000, function(){
+    console.log("server running on port 3000");
+});
