38c222c7404e37677186f8e22a12c86960ee4eba
godson prakasia <godsonprakasia@MacBook-Air.local>
2023-04-01 15:59:20 +0530

    added external module using npm by starting with init npm

--- backendWebDeveloper/introToNode/index.js
+++ backendWebDeveloper/introToNode/index.js
@@ -1,3 +1,13 @@
-//jshint eversion:6 
-const fs = require("fs"); //cannot change value of variable latter
-fs.copyFileSync("file1.txt", "file2.txt");
+// //jshint eversion:6 
+// const fs = require("fs"); //cannot change value of variable latter
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+const superheroes = require("superheroes");
+// var allNames = superheroes.all;
+// console.log(allNames);
+var names = superheroes.random();
+console.log(names);
+
+const supervillain = require("supervillains");
+var villainName= supervillain.random();
+console.log(villainName);
